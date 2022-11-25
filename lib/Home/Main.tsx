8b4909d119c854060f019fc83ac25e9957a926dc
Jakub Schenk <jakub.schenk@post.cz>
2022-11-25 22:41:12 +0100

    v 0.2.1

--- lib/Home/Main.tsx
+++ lib/Home/Main.tsx
@@ -8,7 +8,38 @@ export default function Main(){
 
             </section>
             <section className={styles.content}>
-                
+                <div className={styles.grid}>
+                    <div className={styles.box1}>
+
+                    </div>
+                    <div className={styles.box2}>
+
+                    </div>
+                    <div className={styles.box3}>
+
+                    </div>
+                    <div className={styles.box4}>
+
+                    </div>
+                </div>
+
+                <div className={styles.description}>
+                    
+                    <div className={styles.image}>
+                        <img>
+                        
+                        </img>
+                        <button>
+
+                        </button>
+                    </div>
+                    <div className={styles.text}>
+                        <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                    </div>
+                    
+                </div>
             </section>
         </main>
     )
