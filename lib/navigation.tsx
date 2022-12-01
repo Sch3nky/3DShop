5d355cdde23e2f18857793a121039e082a45d270
Schenky <jakub.schenk@post.cz>
2022-12-01 10:01:50 +0100

    Navigation bar update

--- lib/navigation.tsx
+++ lib/navigation.tsx
@@ -9,23 +9,24 @@ interface Props {
 }
 
 export default function Navigation(props:Props) {
+
     return (
         <div className={style.main}>
-            <div className={style.logo}>
+            <a href="/" className={style.logo}>
                 
-            </div>
+            </a>
 
             <nav className={style.navigation}>
                 <div className={style.otherMobile}>
                     <Link href="/search"><Search color="black"/></Link>
                     <Link href="/cart"><Cart4 color="black"/></Link>
                 </div>
-                <input type="checkbox" name="unfold" id="unfold" hidden/>
+                <input type="checkbox" className={style.unfold} name="unfold" id="unfold" hidden/>
                 <div className={style.links}>
+                    <Link href="/produkty"><h1>Produkty</h1></Link>
+                    <Link href="/sochy"><h1>Sochy</h1></Link>
                     <Link href="https://google.com"><h1>Home</h1></Link>
-                    <Link href="https://google.com"><h1>Home</h1></Link>
-                    <Link href="https://google.com"><h1>Home</h1></Link>
-                    <Link href="https://google.com"><h1>Home</h1></Link>
+                    <Link href="/kontakt"><h1>Kontakt</h1></Link>
 
                 </div>
                 <label className={style.hamburgerMenu} htmlFor="unfold">
@@ -41,7 +42,7 @@ export default function Navigation(props:Props) {
                 </label>
             </nav>
             <div className={style.other}>
-                <Link href="/search"><Search color="black"/></Link>
+                <button><Search color="black"/></button>
                 <Link href="/cart"><Cart4 color="black"/></Link>
             </div>
         </div>
